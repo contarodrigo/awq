55ac2f1eb185dcfcefb8b5a1e92b9d9c629aa0d4
Rodrigo da Silva <contarodrigo@gmail.com>
2020-03-26 22:11:14 -0300

    criacao de uma splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animated_splash/animated_splash.dart';
 import 'package:awq/screens/list_quotes_screen.dart';
 import 'package:awq/screens/quote_detail_screen.dart';
 import 'package:awq/utils/app_routes.dart';
@@ -11,20 +12,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Frases da Sabedoria Antiga',
       theme: ThemeData(
-          primaryColor: Color.fromRGBO(41, 52, 98, 1),
-          accentColor: Colors.amber,
-          fontFamily: 'Quicksand',
+        primaryColor: Color.fromRGBO(41, 52, 98, 1),
+        accentColor: Colors.amber,
+        fontFamily: 'Quicksand',
+      ),
+      home: AnimatedSplash(
+        imagePath: 'assets/images/book-splash.png',
+        home: ListQuotesScreen(),
+        duration: 150,
+        type: AnimatedSplashType.StaticDuration,
       ),
       routes: {
-        AppRoutes.HOME: (context) => ListQuotesScreen(),
-        AppRoutes.LIST_QUOTES : (context) => ListQuotesScreen(),
-        AppRoutes.QUOTE_DETAIL : (context) =>  QuoteDetailScreen(),
+        AppRoutes.LIST_QUOTES: (context) => ListQuotesScreen(),
+        AppRoutes.QUOTE_DETAIL: (context) => QuoteDetailScreen(),
       },
     );
   }
